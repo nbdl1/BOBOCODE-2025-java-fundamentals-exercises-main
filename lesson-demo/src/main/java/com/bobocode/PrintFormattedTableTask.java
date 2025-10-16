39ae7559c609c10f6f99630d693d4617f2f06503
nbdl1 <sergey.bimbash@gmail.com>
2025-10-16 10:13:14 +0300

    PrintFormattedTableTask refactoring

--- lesson-demo/src/main/java/com/bobocode/PrintFormattedTableTask.java
+++ lesson-demo/src/main/java/com/bobocode/PrintFormattedTableTask.java
@@ -2,12 +2,12 @@ package com.bobocode;
 
 
 /*
-*Given an array of strings of unspecified length, write a function that will print its contents in 5 columns. The minimum distance between columns is 4 spaces.
+*Given an array of strings of unspecified length, write a function that will print its contents in 5 COLUMNS. The minimum distance between COLUMNS is 4 spaces.
 
-Please take a look at the following example. Assume the array is called "input" and is passed into the function.
+Please take a look at the following example. Assume the array is called "inputTable" and is passed into the function.
 
 Input:
-String[] input = new String[]{"1", "2", "3", "x", "5", "6", "a", "porosiatko", "c", "10", "11", "12", "13", "14", "15", "16"};
+String[] inputTable = new String[]{"1", "2", "3", "x", "5", "6", "a", "porosiatko", "c", "10", "11", "12", "13", "14", "15", "16"};
 
 Output: (this should be printed to the console)
 
@@ -22,51 +22,55 @@ Output: (this should be printed to the console)
 * */
 public class PrintFormattedTableTask {
 
+    public static final String[] inputTable = new String[]{"1", "2", "3", "x", "5", "6", "a",
+            "porosiatko", "c", "10", "11", "12", "13", "14", "15", "16"};
+    public static final int COLUMNS = 5;
+    public static final int MIN_CELL_PADDING = 4;
+    public static final int[] MAX_CELL_WIDTHS = new int[COLUMNS];
+
     public static void main(String[] args) {
         printFormattedTable();
     }
 
-
-    public static final String[] input = new String[]{"1", "2", "3", "x", "5", "6", "a",
-            "porosiatko", "c", "10", "11", "12", "13", "14", "15", "16"};
-    public static final int columns = 5;
-    public static final int minCellPadding = 4;
     public static void printFormattedTable() {
 
 
-       for(int i = 0; i < input.length; i++) {
-           printElement(input[i], i, getElementColumn(i));
-       }
-    }
-    private static void printElement(String element,int elementPosition, int column){
-        System.out.print(element);
-        printSpaces(
-                getRequiredSpacesForCell(
-                        element,
-                        getMaxCellWidthForCurrentColumn(elementPosition)
-                )
-        );
-        if(elementPosition>0&&((elementPosition+1)%columns)==0){
-            System.out.print("\n");
-        };
-    }
-    private static int getRequiredSpacesForCell(String cellString, int maxCellStringWidthForCurrentColumn) {
-        return maxCellStringWidthForCurrentColumn - cellString.length()+minCellPadding;
-    }
-    private static void printSpaces(int needSpaces){
-        for(int j = 0; j <= needSpaces; j++){
-            System.out.print("\s");
+        getMaxColumnWidths();
+        for (int inputElementIndex = 0; inputElementIndex < inputTable.length; inputElementIndex++) {
+            printElement(inputTable[inputElementIndex], inputElementIndex, getElementColumn(inputElementIndex));
         }
     }
 
-    private static int getMaxCellWidthForCurrentColumn(int elementPosition) {
-        int maxCellWidthForCurrentColumn = 0;
-        for (int i = elementPosition%columns; i < input.length; i=i+columns) {
-            maxCellWidthForCurrentColumn = Math.max(maxCellWidthForCurrentColumn, input[i].length());
+    private static void printElement(String element, int elementPosition, int column) {
+        System.out.print(element);
+        printSpaces(getRequiredSpacesForCell(element, MAX_CELL_WIDTHS[column]));
+        boolean ifCurrentElementIsLastColumnElement = ((elementPosition + 1) % COLUMNS) == 0;
+        if (ifCurrentElementIsLastColumnElement) {
+            System.out.print("\n");
         }
-        return maxCellWidthForCurrentColumn;
     }
+
+    private static int getRequiredSpacesForCell(String cellString, int maxCellStringWidthForCurrentColumn) {
+        return maxCellStringWidthForCurrentColumn - cellString.length() + MIN_CELL_PADDING;
+    }
+
+    private static void printSpaces(int needSpaces) {
+        for (int space = 0; space < needSpaces; space++) {
+            System.out.print(" ");
+        }
+    }
+
+    private static void getMaxColumnWidths() {
+        for (int column = 0; column < COLUMNS; column++) {
+            int maxColumnWidth = 0;
+            for (int elementPosition = column; elementPosition < inputTable.length; elementPosition = elementPosition + COLUMNS) {
+                maxColumnWidth = Math.max(maxColumnWidth, inputTable[elementPosition].length());
+            }
+            MAX_CELL_WIDTHS[column] = maxColumnWidth;
+        }
+    }
+
     private static int getElementColumn(int elementPosition) {
-        return (elementPosition)%columns;
-       }
+        return (elementPosition) % COLUMNS;
+    }
 }
